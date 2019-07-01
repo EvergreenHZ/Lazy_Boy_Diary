0891759624c44d42600b7a41da0f73098351f162
EvergreenHZ <huaizhixtp@168.com>
2019-07-01 22:53:16 +0800

    rio

--- dh_chat/src/common.h
+++ dh_chat/src/common.h
@@ -58,4 +58,25 @@ enum MessageType {
         FILE_TRANSFER
 };
 
+#define RIO_BUFSIZE 8192
+typedef struct {
+        int rio_fd;                /* Descriptor for this internal buf */
+        int rio_cnt;               /* Unread bytes in internal buf */
+        char *rio_bufptr;          /* Next unread byte in internal buf */
+        char rio_buf[RIO_BUFSIZE]; /* Internal buffer */
+} rio_t;
+
+
+// unbuffered
+ssize_t rio_readn(int fd, void *usrbuf, size_t n);
+
+ssize_t rio_writen(int fd, void *usrbuf, size_t n);
+
+
+// buffered
+void rio_readinitb(rio_t *rp, int fd) ;
+
+ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n);
+
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
 #endif
